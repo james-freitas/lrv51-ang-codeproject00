e07bf081d94cb1d2e7296e6c1fd0e1a06d8b94ee
James Freitas <jamesfrj@yahoo.com.br>
2016-02-10 14:43:51 -0200

    Services Project

--- public/build/js/services/project.js
+++ public/build/js/services/project.js
@@ -1,7 +1,10 @@
 angular.module('app.services')
 .service('Project', ['$resource', '$filter', '$httpParamSerializer', 'appConfig',
     function($resource, $filter, $httpParamSerializer, appConfig) {
-    return $resource(appConfig.baseUrl + '/project/:id', {id: '@id'}, {
+    return $resource(appConfig.baseUrl + '/project/:id', {id: '@id'},  {
+        query: {
+            isArray: false
+        },
         save: {
           method: 'POST',
           transformRequest: function (data) {
